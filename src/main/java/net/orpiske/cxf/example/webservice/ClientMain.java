ba3a103d073dd724e9955f8eaee8956ddb973261
Otavio R. Piske <angusyoung@gmail.com>
2013-08-26 20:45:30 -0300

    Modified the code so that it works with the simple client hosted on OpenShift

--- src/main/java/net/orpiske/cxf/example/webservice/ClientMain.java
+++ src/main/java/net/orpiske/cxf/example/webservice/ClientMain.java
@@ -12,7 +12,8 @@ import net.orpiske.examples.cxf.webservice.TimeService;
 
 
 public class ClientMain {
-	
+	private static String ip = "localhost";
+	private static String port = "80";
 
 	public static void callService() {
 		JaxWsProxyFactoryBean factory = new JaxWsProxyFactoryBean();
@@ -22,48 +23,57 @@ public class ClientMain {
 		 */
 		factory.getInInterceptors().add(new LoggingInInterceptor());
 		factory.getOutInterceptors().add(new LoggingOutInterceptor());
-		
-		/*  
+
+		/*
 		 * Sets the service class
 		 */
 		factory.setServiceClass(TimeService.class);
-		
+
 		/*
 		 * Target URL
 		 */
-		
-		factory.setAddress("http://localhost:8080/TimeService");
+
+		factory.setAddress("http://" + ip + ":" + port + "/TimeService");
 
 		TimeService port = (TimeService) factory.create();
-		
-	
+
+
 		/*
 		 * Sets the WSDL URL location
 		 */
 		factory.setWsdlURL("classpath:/wsdl/timeservice/TimeService.wsdl");
 		factory.setServiceName(new QName("http://www.orpiske.net/examples/cxf/webservice",
 				"TimeService"));
-		
+
 		/*
 		 * Call the service and handle the response
 		 */
 		XMLGregorianCalendar ret = port.timeService("12");
-		
+
 		if (ret != null) {
 			System.out.println("Ret = " + ret.toGregorianCalendar().toString());
 		}
 		else {
 			System.out.println("No response from the server");
 		}
-		
+
 	}
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		callService();
+		if (args.length == 1) {
+			ip = args[0];
+		}
+		else {
+			if (args.length >= 2) {
+				ip = args[0];
+				port = args[1];
+			}
+		}
 
+		callService();
 	}
 
 }
